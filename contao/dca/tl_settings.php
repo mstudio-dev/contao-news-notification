2a8e0264f70da55886d390ffdb09ecf2e6e5558c
Markus Schnagl <mail@mstudio.de>
2026-03-16 14:41:27 +0100

    refactor: use PaletteManipulator and typed SQL array in tl_settings DCA

--- contao/dca/tl_settings.php
+++ contao/dca/tl_settings.php
@@ -2,17 +2,23 @@
 
 declare(strict_types=1);
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default']
-    .= ';{mstudio_news_notification_legend},mstudio_news_notification_sender,mstudio_news_notification_recipient';
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+PaletteManipulator::create()
+    ->addLegend('mstudio_news_notification_legend', 'chmod_legend', PaletteManipulator::POSITION_BEFORE)
+    ->addField('mstudio_news_notification_sender', 'mstudio_news_notification_legend', PaletteManipulator::POSITION_APPEND)
+    ->addField('mstudio_news_notification_recipient', 'mstudio_news_notification_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_settings')
+;
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['mstudio_news_notification_sender'] = [
     'inputType' => 'text',
     'eval'      => ['rgxp' => 'email', 'maxlength' => 255, 'tl_class' => 'w50'],
-    'sql'       => "varchar(255) NOT NULL default ''",
+    'sql'       => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
 
 $GLOBALS['TL_DCA']['tl_settings']['fields']['mstudio_news_notification_recipient'] = [
     'inputType' => 'text',
     'eval'      => ['rgxp' => 'email', 'maxlength' => 255, 'tl_class' => 'w50'],
-    'sql'       => "varchar(255) NOT NULL default ''",
+    'sql'       => ['type' => 'string', 'length' => 255, 'default' => ''],
 ];
